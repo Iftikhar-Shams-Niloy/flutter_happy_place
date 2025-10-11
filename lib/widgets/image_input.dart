e32b8f8a5522b41528c0852c3fafed804617e7cb
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-10-11 16:56:12 +0600

    Update image input functionality: add image capture and selection options

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,20 +13,85 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+  void _captureImage() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if (pickedImage == null) {
+      return;
+    } else {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
+  void _chooseImage() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 600,
+    );
+    if (pickedImage == null) {
+      return;
+    } else {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 250,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: Card(
-        borderOnForeground: true,
-        child: TextButton.icon(
-          onPressed: () {},
-          label: const Text("Take Picture"),
-          icon: const Icon(Icons.camera_rounded),
+    Widget content = Text("Nothing to show!");
+    if (_selectedImage != null) {
+      content = ClipRRect(
+        borderRadius: BorderRadius.circular(4),
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
         ),
-      ),
+      );
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 250,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 3),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          child: content,
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Card(
+              child: TextButton.icon(
+                onPressed: _captureImage,
+                label: const Text("Take Picture"),
+                icon: const Icon(Icons.camera_rounded),
+              ),
+            ),
+            Card(
+              child: TextButton.icon(
+                onPressed: _chooseImage,
+                label: const Text("Choose Picture"),
+                icon: const Icon(Icons.image),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
