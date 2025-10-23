858cdefec4a7057530c561f4f503fb228d06b9f4
Iftikhar-Shams-Niloy <niloyshams21@gmail.com>
2025-10-24 00:14:05 +0600

    Implement current location retrieval in LocationInputWidget and update UI accordingly

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInputWidget extends StatefulWidget {
   const LocationInputWidget({super.key});
@@ -10,12 +11,50 @@ class LocationInputWidget extends StatefulWidget {
 }
 
 class _LocationInputWidgetState extends State<LocationInputWidget> {
-  void _getCurrentLocation(){
+  Location? _pickedLocation;
+  var _isGettingLocation = false;
 
+  void _getCurrentLocation() async {
+    Location location = Location();
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isGettingLocation = true;
+    });
+    locationData = await location.getLocation();
+    setState(() {
+      _isGettingLocation = false;
+    });
+    print(locationData.latitude);
+    print(locationData.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      "No location chosen!",
+      textAlign: TextAlign.center,
+    );
+
+    if (_isGettingLocation == true) {
+      previewContent = const CircularProgressIndicator();
+    }
+
     return Column(
       children: [
         Container(
@@ -26,14 +65,14 @@ class _LocationInputWidgetState extends State<LocationInputWidget> {
             border: Border.all(width: 3),
             borderRadius: BorderRadius.all(Radius.circular(8)),
           ),
-          child: Text("No location chosen!", textAlign: TextAlign.center),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Card(
               child: TextButton.icon(
-                onPressed: () {},
+                onPressed: _getCurrentLocation,
                 icon: const Icon(Icons.location_on),
                 label: const Text("Current Location"),
               ),
